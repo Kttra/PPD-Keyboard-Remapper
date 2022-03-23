6d58274e843758a7cc578e3cf6f2eed7385e1130
Kttra <100814612+Kttra@users.noreply.github.com>
2022-03-22 19:37:17 -0700

    Added Assets

---
+++ Assets/sub.pas
@@ -0,0 +1,20 @@
+{
+Glovepie is a programmable input emulator. It is thought to be made for the Wii remote or wiimote but it is not.
+It was originally made to emulate the glove 5 hand controller with the computer. It has expanded very far and can handle many different inputs
+including voice. GlovePIE stands for Glove programmable input emulator. Glovepie is a simple programming language but is very powerul.
+The code in Glovepie can go in almost any order because it’s so simple. It’s coding is very easy to memorize and you could probably have most of it
+memorized in a few days. It was programmed in c++, the base computer programming language, so it can do almost anything and has few limits.
+}
+
+
+unit PIEFree;
+
+interface
+
+uses
+  SysUtils, Classes;
+
+
+implementation
+
+end.
